5831b6efe96a665e65ccdc9f20d875abde94e90e
VALERIYA_MIKHAILOVA <valeriya.mikhailova@mail.ru>
2022-02-17 16:32:12 +0400

    Новое название

--- src/test/java/tests/Cases.java
+++ src/test/java/tests/Cases.java
@@ -8,9 +8,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class Case1 {
+public class Cases {
 
     private static WebDriver driver;
     private static details details;
@@ -21,11 +22,9 @@ public class Case1 {
         driver = new ChromeDriver();
         details = new details(driver);
     }
-
     @Test
     @DisplayName("Проверка операций с целыми числами")
     public void test1() {
-
         driver.get("http://google.com");
         driver.findElement(By.cssSelector("input.gLFyf.gsfi")).sendKeys("calculator", Keys.ENTER);
         driver.findElement(By.cssSelector("[jsname='j93WEe']")).click();
@@ -42,11 +41,29 @@ public class Case1 {
         driver.findElement(By.cssSelector("[jsname='Ax5wH']")).click();
         driver.findElement(By.cssSelector("[jsname='Pt8tGc']")).click();
         assertEquals("1", details.getResult());
-
     }
-
-    @AfterAll
-    public static void teardown () {
-        driver.quit();
+    @Test
+    @DisplayName("Проверка деления на ноль")
+    public void test2() {
+        driver.get("http://google.com");
+        driver.findElement(By.cssSelector("input.gLFyf.gsfi")).sendKeys("calculator", Keys.ENTER);
+        driver.findElement(By.cssSelector("[jsname='abcgof']")).click();
+        driver.findElement(By.cssSelector("[jsname='WxTTNd']")).click();
+        driver.findElement(By.cssSelector("[jsname='bkEvMb']")).click();
+        driver.findElement(By.cssSelector("[jsname='Pt8tGc']")).click();
+        assertEquals("Infinity", details.getInfinity());
+    }
+    @Test
+    @DisplayName("Проверка ошибки при отсутствии значения")
+    public void test3() {
+        driver.get("http://google.com");
+        driver.findElement(By.cssSelector("input.gLFyf.gsfi")).sendKeys("calculator", Keys.ENTER);
+        driver.findElement(By.cssSelector("[jsname='aN1RFf']")).click();
+        driver.findElement(By.cssSelector("[jsname='Pt8tGc']")).click();
+        assertEquals("Error", details.getError());
+    }
+        @AfterAll
+        public static void teardown () {
+            driver.quit();
     }
 }
