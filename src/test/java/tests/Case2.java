01f66bfe5fb90b8a99b5f011d02de6872e730673
VALERIYA_MIKHAILOVA <valeriya.mikhailova@mail.ru>
2022-02-17 16:29:05 +0400

    Объединение классов в один файл

--- src/test/java/tests/Case2.java
+++ src/test/java/tests/Case2.java
@@ -22,10 +22,29 @@ public class Case2 {
         driver = new ChromeDriver();
         details = new details(driver);
     }
-
+    @Test
+    @DisplayName("Проверка операций с целыми числами")
+    public void test1() {
+        driver.get("http://google.com");
+        driver.findElement(By.cssSelector("input.gLFyf.gsfi")).sendKeys("calculator", Keys.ENTER);
+        driver.findElement(By.cssSelector("[jsname='j93WEe']")).click();
+        driver.findElement(By.cssSelector("[jsname='N10B9']")).click();
+        driver.findElement(By.cssSelector("[jsname='XSr6wc']")).click();
+        driver.findElement(By.cssSelector("[jsname='lVjWed']")).click();
+        driver.findElement(By.cssSelector("[jsname='qCp9A']")).click();
+        driver.findElement(By.cssSelector("[jsname='YovRWb']")).click();
+        driver.findElement(By.cssSelector("[jsname='KN1kY']")).click();
+        driver.findElement(By.cssSelector("[jsname='pPHzQc']")).click();
+        driver.findElement(By.cssSelector("[jsname='xAP7E']")).click();
+        driver.findElement(By.cssSelector("[jsname='bkEvMb']")).click();
+        driver.findElement(By.cssSelector("[jsname='WxTTNd']")).click();
+        driver.findElement(By.cssSelector("[jsname='Ax5wH']")).click();
+        driver.findElement(By.cssSelector("[jsname='Pt8tGc']")).click();
+        assertEquals("1", details.getResult());
+    }
     @Test
     @DisplayName("Проверка деления на ноль")
-    public void test1() {
+    public void test2() {
         driver.get("http://google.com");
         driver.findElement(By.cssSelector("input.gLFyf.gsfi")).sendKeys("calculator", Keys.ENTER);
         driver.findElement(By.cssSelector("[jsname='abcgof']")).click();
@@ -33,6 +52,15 @@ public class Case2 {
         driver.findElement(By.cssSelector("[jsname='bkEvMb']")).click();
         driver.findElement(By.cssSelector("[jsname='Pt8tGc']")).click();
         assertEquals("Infinity", details.getInfinity());
+    }
+    @Test
+    @DisplayName("Проверка ошибки при отсутствии значения")
+    public void test3() {
+        driver.get("http://google.com");
+        driver.findElement(By.cssSelector("input.gLFyf.gsfi")).sendKeys("calculator", Keys.ENTER);
+        driver.findElement(By.cssSelector("[jsname='aN1RFf']")).click();
+        driver.findElement(By.cssSelector("[jsname='Pt8tGc']")).click();
+        assertEquals("Error", details.getError());
     }
         @AfterAll
         public static void teardown () {
