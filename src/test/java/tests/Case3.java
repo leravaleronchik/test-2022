5a6632037cc06b6b15a487477d190cc606934777
VALERIYA_MIKHAILOVA <valeriya.mikhailova@mail.ru>
2022-02-17 16:14:40 +0400

    Добавлена проверка для кейса "Ошибка при отсутствии значения"

--- src/test/java/tests/Case3.java
+++ src/test/java/tests/Case3.java
@@ -9,14 +9,18 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class Case3 {
 
     private static WebDriver driver;
+    private static details details;
 
     @BeforeAll
     public static void init () {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         driver = new ChromeDriver();
+        details = new details(driver);
     }
     @Test
     @DisplayName("Проверка ошибки при отсутствии значения")
@@ -25,6 +29,7 @@ public class Case3 {
         driver.findElement(By.cssSelector("input.gLFyf.gsfi")).sendKeys("calculator", Keys.ENTER);
         driver.findElement(By.cssSelector("[jsname='aN1RFf']")).click();
         driver.findElement(By.cssSelector("[jsname='Pt8tGc']")).click();
+        assertEquals("Error", details.getError());
     }
         @AfterAll
         public static void teardown () {
--- src/test/java/tests/details.java
+++ src/test/java/tests/details.java
@@ -9,11 +9,16 @@ import static org.openqa.selenium.support.PageFactory.initElements;
 public class details {
     @FindBy(css="div[class=\"jlkklc\"] #cwos.qv3Wpe")
     public WebElement Infinity;
+    @FindBy(css="div[class=\"jlkklc\"] #cwos.qv3Wpe")
+    public WebElement Error;
 
        public details(WebDriver driver) {
         initElements(driver, this);
     }
     public String getInfinity() {
         return Infinity.getText();
+    }
+    public String getError() {
+        return Error.getText();
     }
 }
